1f6f8a75ce69176459030f06b318deda1a1d87aa
tiendat2302 <tiendatvt230203@gmail.com>
2026-01-14 14:23:10 +0700

    update

--- tunnel.bpf.c
+++ tunnel.bpf.c
@@ -1,55 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/bpf.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_endian.h>
-
-/* xsks_map[0] = AF_XDP socket */
-struct {
-    __uint(type, BPF_MAP_TYPE_XSKMAP);
-    __uint(max_entries, 1);
-    __type(key, __u32);
-    __type(value, __u32);
-} xsks_map SEC(".maps");
-
-/* config[0] = remote_net, config[1] = remote_mask */
-struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 2);
-    __type(key, __u32);
-    __type(value, __u32);
-} config SEC(".maps");
 
 SEC("xdp")
-int xdp_redirect(struct xdp_md *ctx)
+int xdp_drop_all(struct xdp_md *ctx)
 {
-    void *data = (void *)(long)ctx->data;
-    void *end  = (void *)(long)ctx->data_end;
-
-    struct ethhdr *eth = data;
-    if ((void *)(eth + 1) > end)
-        return XDP_PASS;
-
-    if (eth->h_proto != bpf_htons(ETH_P_IP))
-        return XDP_PASS;
-
-    struct iphdr *ip = (void *)(eth + 1);
-    if ((void *)(ip + 1) > end)
-        return XDP_PASS;
-
-    __u32 k = 0;
-    __u32 *rnet = bpf_map_lookup_elem(&config, &k);
-    k = 1;
-    __u32 *rmask = bpf_map_lookup_elem(&config, &k);
-    if (!rnet || !rmask)
-        return XDP_PASS;
-
-    if ((bpf_ntohl(ip->daddr) & *rmask) != (*rnet & *rmask))
-        return XDP_PASS;
-
-    __u32 idx = 0;  // 1 LAN queue
-    return bpf_redirect_map(&xsks_map, idx, XDP_PASS);
+    return XDP_DROP;
 }
 
 char LICENSE[] SEC("license") = "GPL";
--- tunnel_loader.c
+++ tunnel_loader.c
@@ -1,125 +1,72 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <signal.h>
-#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 #include <net/if.h>
-#include <sys/socket.h>
-#include <linux/if_packet.h>
-#include <linux/if_ether.h>
-#include <xdp/xsk.h>
+#include <errno.h>
 
-#define NUM_FRAMES 2048
-#define FRAME_SIZE XSK_UMEM__DEFAULT_FRAME_SIZE
-#define BATCH      32
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
 
 static volatile int running = 1;
-static void sig(int s){ (void)s; running = 0; }
+static void sig(int s) { (void)s; running = 0; }
 
-static void die(const char *msg) {
-    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-    exit(1);
-}
-
-int main()
+int main(int argc, char **argv)
 {
+    if (argc != 3) {
+        printf("Usage: %s <iface> <bpf.o>\n", argv[0]);
+        return 1;
+    }
+
+    char *ifname = argv[1];
+    char *bpf_obj = argv[2];
+
     signal(SIGINT, sig);
     signal(SIGTERM, sig);
 
-    /* ===== CONFIG ===== */
-    const char *lan_if = "enp7s0";
-    const char *wan_if = "enp5s0";   // ÉP CỨNG WAN
-    int wan_ifindex = if_nametoindex(wan_if);
-    if (wan_ifindex == 0) die("if_nametoindex(wan) failed");
-
-    /* ===== RAW SOCKET WAN ===== */
-    int wan_fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-    if (wan_fd < 0) die("socket(AF_PACKET) failed");
-
-    struct sockaddr_ll wan_addr = {
-        .sll_family   = AF_PACKET,
-        .sll_ifindex  = wan_ifindex,
-        .sll_protocol = htons(ETH_P_ALL),
-    };
-    if (bind(wan_fd, (void*)&wan_addr, sizeof(wan_addr)) < 0) die("bind(wan_fd) failed");
-
-    /* ===== AF_XDP LAN ===== */
-    void *buf = NULL;
-    if (posix_memalign(&buf, getpagesize(), (size_t)NUM_FRAMES * FRAME_SIZE) != 0) {
-        fprintf(stderr, "posix_memalign failed\n");
+    int ifindex = if_nametoindex(ifname);
+    if (!ifindex) {
+        perror("if_nametoindex");
         return 1;
     }
 
-    struct xsk_umem *umem = NULL;
-    struct xsk_ring_prod fq;
-    struct xsk_ring_cons cq;   /* phải có CQ */
-    struct xsk_ring_cons rx;
-
-    struct xsk_umem_config uc = {
-        .fill_size = NUM_FRAMES,
-        .comp_size = NUM_FRAMES,
-        .frame_size = FRAME_SIZE,
-        .frame_headroom = 0,
-    };
-    if (xsk_umem__create(&umem, buf, (size_t)NUM_FRAMES * FRAME_SIZE, &fq, &cq, &uc) != 0) {
-        fprintf(stderr, "xsk_umem__create failed\n");
+    struct bpf_object *obj = bpf_object__open_file(bpf_obj, NULL);
+    if (libbpf_get_error(obj)) {
+        fprintf(stderr, "open bpf obj failed\n");
         return 1;
     }
 
-    struct xsk_socket *xsk = NULL;
-    struct xsk_socket_config xc = {
-        .rx_size = NUM_FRAMES,
-        .tx_size = 0,
-        .libbpf_flags = XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD,
-        .bind_flags = 0,
-    };
-    if (xsk_socket__create(&xsk, lan_if, 0, umem, &rx, NULL, &xc) != 0) {
-        fprintf(stderr, "xsk_socket__create failed\n");
+    if (bpf_object__load(obj)) {
+        fprintf(stderr, "load bpf obj failed\n");
         return 1;
     }
 
-    /* fill ring lần đầu */
-    uint32_t idx;
-    int reserved = xsk_ring_prod__reserve(&fq, NUM_FRAMES, &idx);
-    if (reserved != NUM_FRAMES) {
-        fprintf(stderr, "fq reserve failed: reserved=%d\n", reserved);
+    struct bpf_program *prog =
+        bpf_object__find_program_by_name(obj, "xdp_drop_all");
+    if (!prog) {
+        fprintf(stderr, "cannot find xdp program\n");
         return 1;
     }
-    for (int i = 0; i < NUM_FRAMES; i++)
-        *xsk_ring_prod__fill_addr(&fq, idx + i) = (uint64_t)i * FRAME_SIZE;
-    xsk_ring_prod__submit(&fq, NUM_FRAMES);
 
-    printf("RUNNING: LAN %s -> WAN %s (FORCED)\n", lan_if, wan_if);
+    int prog_fd = bpf_program__fd(prog);
 
-    /* ===== LOOP ===== */
-    while (running) {
-        uint32_t rx_idx;
-        uint32_t rcvd = xsk_ring_cons__peek(&rx, BATCH, &rx_idx);
-        if (!rcvd) { usleep(1000); continue; }
-
-        for (uint32_t i = 0; i < rcvd; i++) {
-            uint64_t addr = xsk_ring_cons__rx_desc(&rx, rx_idx + i)->addr;
-            uint32_t len  = xsk_ring_cons__rx_desc(&rx, rx_idx + i)->len;
-
-            /* addr có thể chứa offset bits -> extract cho an toàn */
-            uint64_t base = xsk_umem__extract_addr(addr);
-            void *pkt = xsk_umem__get_data(buf, base);
-
-            /* SEND THẲNG RA enp5s0 */
-            (void)sendto(wan_fd, pkt, len, 0, (void*)&wan_addr, sizeof(wan_addr));
-
-            /* trả frame: phải check reserve */
-            uint32_t f;
-            if (xsk_ring_prod__reserve(&fq, 1, &f) == 1) {
-                *xsk_ring_prod__fill_addr(&fq, f) = base;
-                xsk_ring_prod__submit(&fq, 1);
-            }
-        }
-
-        xsk_ring_cons__release(&rx, rcvd);
+    if (bpf_set_link_xdp_fd(ifindex, prog_fd, 0) < 0) {
+        perror("attach xdp");
+        return 1;
     }
 
+    printf("XDP DROP attached on %s\n", ifname);
+    printf("Ctrl+C to detach\n");
+
+    while (running)
+        sleep(1);
+
+    bpf_set_link_xdp_fd(ifindex, -1, 0);
+    printf("XDP detached\n");
+
+    bpf_object__close(obj);
     return 0;
 }
 
