26c1cb46abbd0ef4d9b61d582a3b3ffdce268267
tiendat2302 <tiendatvt230203@gmail.com>
2026-01-14 14:32:38 +0700

    update

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 CLANG := clang
 CC := gcc
 
+BPF_CFLAGS := -O2 -g -target bpf -D__TARGET_ARCH_x86
+USER_CFLAGS := -O2 -g -Wall
+USER_LDFLAGS := -lbpf
+
 all: tunnel.bpf.o tunnel_xdp
 
 tunnel.bpf.o: tunnel.bpf.c
-	$(CLANG) -O2 -target bpf -c $< -o $@
+	$(CLANG) $(BPF_CFLAGS) -c $< -o $@
 
 tunnel_xdp: tunnel_xdp.c
-	$(CC) -O2 -g $< -o $@ -lbpf
+	$(CC) $(USER_CFLAGS) $< -o $@ $(USER_LDFLAGS)
 
 clean:
 	rm -f tunnel.bpf.o tunnel_xdp
--- tunnel_loader.c
+++ tunnel_loader.c
@@ -10,61 +10,84 @@
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 
+#define MAX_IF 16
+
 static volatile int running = 1;
 static void sig(int s) { (void)s; running = 0; }
 
+static void die(const char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
 int main(int argc, char **argv)
 {
-    if (argc != 3) {
-        printf("Usage: %s <iface> <bpf.o>\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <bpf.o> <wan_if1> [wan_if2] [wan_if3]...\n", argv[0]);
+        fprintf(stderr, "Example: %s ./tunnel.bpf.o enp4s0 enp5s0 enp6s0\n", argv[0]);
         return 1;
     }
 
-    char *ifname = argv[1];
-    char *bpf_obj = argv[2];
+    const char *bpf_obj = argv[1];
+    int n_if = argc - 2;
+    if (n_if > MAX_IF) die("too many interfaces");
 
     signal(SIGINT, sig);
     signal(SIGTERM, sig);
 
-    int ifindex = if_nametoindex(ifname);
-    if (!ifindex) {
-        perror("if_nametoindex");
-        return 1;
-    }
-
+    /* load BPF */
     struct bpf_object *obj = bpf_object__open_file(bpf_obj, NULL);
-    if (libbpf_get_error(obj)) {
-        fprintf(stderr, "open bpf obj failed\n");
-        return 1;
-    }
+    if (libbpf_get_error(obj)) die("bpf_object__open_file failed");
+    if (bpf_object__load(obj)) die("bpf_object__load failed");
 
-    if (bpf_object__load(obj)) {
-        fprintf(stderr, "load bpf obj failed\n");
-        return 1;
-    }
-
-    struct bpf_program *prog =
-        bpf_object__find_program_by_name(obj, "xdp_drop_all");
+    struct bpf_program *prog = bpf_object__find_program_by_name(obj, "xdp_drop_all");
     if (!prog) {
-        fprintf(stderr, "cannot find xdp program\n");
+        fprintf(stderr, "Cannot find program 'xdp_drop_all'\n");
         return 1;
     }
-
     int prog_fd = bpf_program__fd(prog);
+    if (prog_fd < 0) die("bpf_program__fd failed");
 
-    if (bpf_set_link_xdp_fd(ifindex, prog_fd, 0) < 0) {
-        perror("attach xdp");
-        return 1;
+    int ifindex[MAX_IF] = {0};
+    __u32 flags_used[MAX_IF] = {0};
+
+    /* attach XDP to each WAN interface */
+    for (int i = 0; i < n_if; i++) {
+        const char *ifname = argv[i + 2];
+        ifindex[i] = if_nametoindex(ifname);
+        if (!ifindex[i]) {
+            fprintf(stderr, "if_nametoindex(%s) failed\n", ifname);
+            continue;
+        }
+
+        __u32 flags = 0; /* native */
+        if (bpf_set_link_xdp_fd(ifindex[i], prog_fd, flags) < 0) {
+            /* fallback generic */
+            flags = XDP_FLAGS_SKB_MODE;
+            if (bpf_set_link_xdp_fd(ifindex[i], prog_fd, flags) < 0) {
+                fprintf(stderr, "Attach XDP failed on %s: %s\n", ifname, strerror(errno));
+                ifindex[i] = 0;
+                continue;
+            }
+        }
+
+        flags_used[i] = flags;
+        printf("XDP DROP attached on %s (ifindex=%d) mode=%s\n",
+               ifname, ifindex[i], flags ? "generic" : "native");
     }
 
-    printf("XDP DROP attached on %s\n", ifname);
-    printf("Ctrl+C to detach\n");
+    printf("Running... Ctrl+C to detach\n");
+    while (running) sleep(1);
 
-    while (running)
-        sleep(1);
-
-    bpf_set_link_xdp_fd(ifindex, -1, 0);
-    printf("XDP detached\n");
+    /* detach */
+    for (int i = 0; i < n_if; i++) {
+        if (!ifindex[i]) continue;
+        if (bpf_set_link_xdp_fd(ifindex[i], -1, flags_used[i]) < 0) {
+            fprintf(stderr, "Detach failed ifindex=%d: %s\n", ifindex[i], strerror(errno));
+        }
+    }
+    printf("Detached.\n");
 
     bpf_object__close(obj);
     return 0;
